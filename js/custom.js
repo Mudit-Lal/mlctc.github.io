ccec5bbabe2352ffa2548b0bc0fa712db88dc4f4
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-04-25 23:33:09 +0530

    Form Validation Added

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,46 @@
-//[Custom Javascript]
 
-//Project:	Law Firm - Multipage Html Responsive Template
-//Version:	1.1
-//Primary use:	Law Firm - Multipage Html Responsive Template 
 
-//add your script here
 
+var submitted=false;
+function clientvalidateForm() {
+    var fullname = document.forms["query"]["entry.2005620554"].value;
+    var phno = document.forms["query"]["entry.1166974658"].value;
+    var email = document.forms["query"]["entry.1045781291"].value;
+    var sub = document.forms["query"]["entry.288240117"].value;
+    var query = document.forms["query"]["entry.839337160"].value;
+    var emailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+
+    if (fullname == "") {
+        alert("Full name must be filled out");
+        return false;
+      }
+      if (phno == "") {
+        alert("Phone Number must be filled out");
+        return false;
+      }
+      if (sub == "") {
+        alert("Subject must be filled out");
+        return false;
+      }
+      if (query == "") {
+        alert("Query cannot be left empty");
+        return false;
+      }
+      if (emailformat.test(email) == false)
+      {
+            alert('Invalid Email Address');
+            return false;
+      }
+
+}
+
+function finalvalidate(){
+  if(clientvalidateForm() == false)
+  {
+    submitted=false;
+  }
+  else{
+        submitted=true;
+      }
+    }
